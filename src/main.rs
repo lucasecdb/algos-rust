61f167786bbb65d196c8d941b9333cd576ad7a51
Lucas Cordeiro <ecdb.lucas@gmail.com>
2021-05-15 00:10:57 -0300

    Add base merge sort

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+use sorting_rust::merge_sort;
+
+fn main() {
+    let mut args = env::args();
+
+    args.next();
+
+    let str_array = args.next().expect("First argument is required");
+
+    let mut array = vec![];
+
+    for num in str_array.split(',').collect::<Vec<_>>() {
+        let num: i32 = match num.parse() {
+            Ok(val) => val,
+            Err(_) => continue,
+        };
+
+        array.push(num);
+    }
+
+    println!("sorted {:?}", merge_sort(&array));
+}
